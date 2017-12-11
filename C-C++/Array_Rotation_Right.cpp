f25c041d084291b7f28610cbacf2d98f44a04075
sibasish14 <sibasish.ghosh14@gmail.com>
2017-12-12 01:25:43 +0530

    --ammend

--- C-C++/Array_Rotation_Right.cpp
+++ C-C++/Array_Rotation_Right.cpp
@@ -1,25 +1,33 @@
 #include <iostream>
 #include<stdio.h>
+
 using namespace std;
 
 int main()
 {
    long a[100000];
    int size,rotations,j=0,testCases;
+   
    cin>>testCases;
-   while(testCases>0){
-		   cout<<"\nEnter size and number of rotations : ";
-       cin>>size>>rotations;
-       cout<<"Enter elements : ";
-       j = rotations % size;
-       for(int i=0;i<size;i++) {
-           cin>>a[j];
-           j=(j+1)%size;
-       }
-       for(int i=0;i<size;i++)
-           cout<<a[i]<<" ";
-cout<<"\n";
-testCases--;
-}
-return 0;
+   
+   while(testCases>0)
+   {
+	cout<<"\nEnter size and number of rotations : ";
+        cin>>size>>rotations;
+   
+        cout<<"Enter elements : ";
+        j = rotations % size;
+        for(int i=0;i<size;i++) 
+        {
+             cin>>a[j];
+             j=(j+1)%size;
+        }
+       
+        for(int i=0;i<size;i++)
+             cout<<a[i]<<" ";
+        cout<<"\n";
+        
+	testCases--;
+   }
+   return 0;
 }
