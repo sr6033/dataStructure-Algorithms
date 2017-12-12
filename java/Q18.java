3352262e234ab9a38d01cf57fa2e1ac8b0bf5d48
DEBASISH SAHOO <33368759+debck@users.noreply.github.com>
2017-12-12 21:10:20 +0530

    Update Q18.java

--- java/Q18.java
+++ java/Q18.java
@@ -17,9 +17,10 @@ class Q18
 		System.out.println("Enter the radius: ");
 		double r = sc.nextDouble();
 
-		double d = (x-x0) + (y - y0);
+		double d = ((x-x0)*(x-x0)) + ((y - y0)*(y - y0));
 		d = Math.sqrt(d);
-
+		if(r==d)
+			System.out.println("the point lies on the circle.");
 		if(r > d)
 			System.out.println("The point lies within the circle.");
 		else
