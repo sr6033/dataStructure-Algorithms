e53ce8ab8be9257d0348bce9b01434a12422cd89
DEBASISH SAHOO <33368759+debck@users.noreply.github.com>
2017-12-12 19:40:33 +0530

    Update Q11.java

--- java/Q11.java
+++ java/Q11.java
@@ -2,10 +2,10 @@ import java.util.*;
 class Q9
 {
 	public static void main(String[] args) throws InputMismatchException
-	{
+	{	System.out.println("Enter no of terms:");
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		
+		System.out.println("Enter choice");
 		System.out.println("1. Display odd divisors");
 		System.out.println("2. Display even divisors");
 		
@@ -14,19 +14,17 @@ class Q9
 		for(int i = 1; i < n; i++)
 		{
 			if(n % i == 0)
-			{
-				if(ch == 1)
-				{
+			{	switch(ch)
+			       {case 1:	
 					if(i % 2 != 0)
 						System.out.print(i + " ");
-				}
+					break;
 				
-				if(ch == 2)
-				{
+				case 2:
 					if(i % 2 == 0)
 						System.out.print(i + " ");
-				}
-			}
+					break;
+			}}
 		}
 		System.out.println();
 	}
