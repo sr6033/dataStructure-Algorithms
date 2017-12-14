73b8eb0e67cb38d4475bc93e1135faab0f7be266
Jevesh Pratap Singh <jevesh8042@gmail.com>
2017-12-14 17:01:24 +0530

    Updated LIS

--- C-C++/Longest_Increasing_Subsequence.cpp
+++ C-C++/Longest_Increasing_Subsequence.cpp
@@ -1,3 +1,6 @@
+//The Longest Increasing Subsequence (LIS) problem is to find the length of the longest subsequence
+//of a given sequence such that all elements of the subsequence are sorted in increasing order.
+//Time Complexity : O(n^2)
 #include<bits/stdc++.h>
 
 using namespace std;
